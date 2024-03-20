cccb12a68c15d20ffc8a774555ce0e962409aef0
chienvuminhph43391 <162954413+chienvuminhph43391@users.noreply.github.com>
2024-03-20 23:15:26 +0700

    login

---
+++ client/view/users/login.php
@@ -0,0 +1,39 @@
+
+    <body>
+<div class="wrapper">
+        <div class="container">
+             <form class="login-form shadow" action="" method="POST">
+                  <h2 class="form-heading">Đăng nhập</h2>
+                  <p class="form-desc">Bạn không có tài khoản ?
+                       <a class="text-link" href="?act=register">Đăng ký</a>
+                  </p>   
+                  <div class="form-control">
+                    <label for="username" class="form-label">Email</label>
+                    <input type="text" class="form-control" id="email" name="email" placeholder="Nhập tên đăng nhập">
+                  </div>
+                  <div class="form-control">
+                    <label for="password" class="form-label">Mật khẩu</label>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Nhập mật khẩu">
+                  </div>
+                 
+                    <div class="form-check">
+                      <input type="checkbox" class="form-check-input" id="rememberMe">
+                      <label class="form-check-label" for="rememberMe">Ghi nhớ đăng nhập</label>
+                    
+                  </div>
+                    <div class="d-flex justify-content-end mt-3 " >
+                      <a href="#" >Quên mật khẩu?</a>
+                    </div>
+                  <div class="d-grid">
+                    <button type="submit" name="submit" class="form-btn btn-success">Đăng Nhập</button>
+                  </div>
+
+              
+             </form>
+        </div>
+   </div>
+    </body>
+
+       
+
+  
