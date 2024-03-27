b59da5a762ff6062f85faa35db0359cc9f9905cf
chienvuminhph43391 <162954413+chienvuminhph43391@users.noreply.github.com>
2024-03-27 19:24:00 +0700

    index admin

---
+++ admin/user/list.php
@@ -0,0 +1,21 @@
+<table class="table mt-4">
+<br>
+     <br>
+     <h2 class="text-center text-primary mt-3">List User</h2>
+     <thead>
+     <tr>
+          <th scope="col">#</th>   
+          <th scope="col">Name</th>
+          <th scope="col">Avatar</th>
+          <th scope="col">Tel</th>
+          <th scope="col">Role</th>
+          <th scope="col">Action</th>
+     </tr>
+     </thead>
+     <tbody>
+     <tr>
+     </tr>
+     
+     </tbody>
+</table>
+<a href="index.php?act=add-category" class="btn btn-primary">Add User</a>
