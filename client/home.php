d761f28d6aa6c2617bd45a4a89d5ce1d9505cd5e
chienvuminhph43391 <162954413+chienvuminhph43391@users.noreply.github.com>
2024-03-29 00:51:58 +0700

    á

--- client/home.php
+++ client/home.php
@@ -2,20 +2,27 @@
 <body>
       
 <div class="container">
+  <!-- Phần banner  -->
   <div class="row">
-    <div class="col-md-8">
+    <div class="col">
       <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <ol class="carousel-indicators">
           <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
           <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
           <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
         </ol>
-        <div class="carousel-inner">
+        <div class=" carousel-inner">
           <div class="carousel-item active">
-            <img src="../img/banner.jpg" class="d-block w-100" alt="Image 1" >
+            <img src="https://theme.hstatic.net/200000278317/1000929405/14/slideshow_1.jpg?v=1170" class="d-block w-100" alt="Image 1" >
           </div>
           <div class="carousel-item">
-            <img src="../img/banner3.jpg" class="d-block w-100" alt="Image 2" >
+            <img src="https://theme.hstatic.net/200000278317/1000929405/14/slideshow_4.jpg?v=1170" class="d-block w-100" alt="Image 2" >
+          </div>
+          <div class="carousel-item">
+            <img src="https://theme.hstatic.net/200000278317/1000929405/14/slideshow_7.jpg?v=1170" class="d-block w-100" alt="Image 3" >
+          </div>
+          <div class="carousel-item">
+            <img src="https://theme.hstatic.net/200000278317/1000929405/14/slideshow_7.jpg?v=1170" class="d-block w-100" alt="Image 3" >
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
@@ -28,18 +35,13 @@
         </a>
       </div>
     </div>
-    <div class="col-md-4">
-      <div class="banner">
-        <img src="../img/banner4.jpg" class="d-block w-100" alt="Banner 1" style="height: 160px;">
-      </div>
-      <div class="banner">
-        <img src="../img/banner1.png" class="d-block w-100" alt="Banner 2"  style="height: 160px;">
-      </div>
-    </div>
+    
   </div>
+
   <hr>
 </div> <br>
 <br> 
+<!-- hết banner  -->
 
 
 <div class="service-area">
@@ -92,21 +94,21 @@
 require_once '../model/List.php';
 $listProduct = top6Product();
 foreach($listProduct as $sp){ ?>
-    <div class="col-4 border p-3 bg-white d-flex justify-content-center">
+    <div class="col-lg-3 col-md-4 col-3 pt-3 pb-3 bg-light ">
 <form action="?act=add-to-cart" method="POST"  enctype="multipart/form-data">
-      <div class="product">
+      <div class="product " >
         <input type="hidden" name="id_sp" value="<?=$sp['id_sp']?>">
-        <img src="../img/<?=$sp['image_sp']?>" alt="Product 1" >
+        <img src="../img/<?=$sp['image_sp']?>" alt="Product 1" style="min-height: 270px; max-width:300px;" >
         <input type="hidden" name="image_sp" value="<?=$sp['image_sp']?>">
         <input type="hidden" name="soluong" value="1" >
         <div class="overlay">
           <div class="content">
-            <h3><?=$sp['name_sp']?></h3>
+            <p><?=$sp['name_sp']?></p>
             <input type="hidden" name="name_sp" value="<?=$sp['name_sp']?>">
-            <p> $<?=$sp['price_sp']?> </p>
+            <p class="text-danger fw-bold"> <?=$sp['price_sp']?>₫ </p>
             <input type="hidden" name="price_sp" value="<?=$sp['price_sp']?>">
-           <a href="?act=add-to-cart"> <button type="submit" name="addToCart" class=" bg-success text-light rounded p-1" >Add to cart </button></a>
-      <a href="?act=viewProduct&id_sp=<?=$sp['id_sp']?>" class="btn bg-dark text-light rounded p-1 ">View Details</a>
+            <button class="add-to-cart btn-success text-light rounded shadow " style="height: 33px;">Add To Cart</button>
+            <a href="index.php?act=viewProduct&id_sp=<?=$sp['id_sp']?>&id_dm=<?=$sp['id_dm']?>"style="height: 33px;" class="btn bg-dark text-light rounded p-1 ">View Details</a>
           </div>
         </div>
       </div>
