d34b58049175a5284abe936b86cc05ee8dd8a681
chienvuminhph43391 <162954413+chienvuminhph43391@users.noreply.github.com>
2024-04-03 13:11:29 +0700

    ác

--- admin/products/list.php
+++ admin/products/list.php
@@ -1,12 +1,25 @@
+<br>
+<br>
+<br>
+<br>
+
+<h2 class="text-center text-primary mt-3">Danh sách sản phẩm</h2>
+<form class="price-slider-amount" method="POST" action="?act=search">
+   <div class="label-input">
+      <input type="text" name="content" placeholder="Tìm kiếm" />
+      <button class="filter-btn" name="search" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+   </div>
+</form>
+
 <table class="table mt-4">
-     <br>
-     <br>
-     <h2 class="text-center text-primary mt-3">List Products</h2>
+   
+
      <thead>
      <tr>
           <th scope="col">#</th>   
           <th scope="col">Tên sản phẩm</th>
           <th scope="col">Ảnh</th>
+          <th scope="col">Danh mục</th>
           <th scope="col">Giá</th>
           <th scope="col">Số lượng</th>
           <th scope="col">Hành động</th>
@@ -23,11 +36,13 @@
           <td scope="row"><?=$pro['id_sp']?></td>
           <td scope="row"><?=$pro['name_sp']?></td>
           <td scope="row"><img src="../img/<?=$pro['image_sp']?>" alt="Ảnh sản phẩm" width="70px"></td>
-          <td scope="row"><?=$pro['price_sp']?></td>
+          <td scope="row"><?=$pro['name_dm']?></td>
+          <td scope="row"><?=number_format((int)$pro['price_sp'], 0, ",", ".")?>₫</td>
           <td scope="row"><?=$pro['soluong']?></td>
+          
           <td scope="row">
-          <a href="<?=$sua?>"><button type="button" class="btn btn-warning" name="edit-category"><i class="fa-solid fa-wrench fa-l"></i> Sửa</button></a>
-              <a href="<?=$xoa?>"><button type="button" class="btn btn-danger" name="deletecate" onclick="return confirm('Những sản phẩm trong danh mục này cũng sẽ bị xóa, xác nhận ?')"><i class="fa-solid fa-trash fa-l"></i> Xóa</button></a>
+          <a href="<?=$sua?>"><button type="button" class="btn btn-warning" name="edit-category"><i class="fa-solid fa-pen"></i> </button></a>
+              <a href="<?=$xoa?>"><button type="button" class="btn btn-danger" name="deletecate" onclick="return confirm('Những sản phẩm trong danh mục này cũng sẽ bị xóa, xác nhận ?')"><i class="fa-solid fa-trash fa-l"></i> </button></a>
 
           </td>
         </tr>
